025b467dbb7769465442120c2a44d82d0900bc4f
hprxkbrddd <egorushkinnikita65@gmail.com>
2025-04-19 20:35:04 +0300

    circullar stuff1

--- src/main/java/com/example/sb_neo4j/dto/TaskDTO.java
+++ src/main/java/com/example/sb_neo4j/dto/TaskDTO.java
@@ -3,10 +3,8 @@ package com.example.sb_neo4j.dto;
 import java.util.List;
 
 public record TaskDTO(
-        Long id,
         String title,
         String description,
-        List<String> skillsRequired,
         String status
         //TODO подвязать enum
 ) {
