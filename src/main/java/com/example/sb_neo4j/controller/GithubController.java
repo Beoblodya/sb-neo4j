d5d3e3934fe5033880858e72f2ffe208f4259416
hprxkbrddd <egorushkinnikita65@gmail.com>
2025-04-20 20:43:06 +0300

    combined fetching data and parsing  result: endpoint returns parsed data

--- src/main/java/com/example/sb_neo4j/controller/GithubController.java
+++ src/main/java/com/example/sb_neo4j/controller/GithubController.java
@@ -5,6 +5,7 @@ import com.example.sb_neo4j.model.Task;
 import com.example.sb_neo4j.service.GithubService;
 import com.example.sb_neo4j.util.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,14 +34,24 @@ public class GithubController {
     }
 
     @GetMapping("/get-issues/{owner}/{repo}/{projectNumber}")
-    public Mono<ResponseEntity<Map>> getIssues(
+    public Mono<ResponseEntity<List<TaskDTO>>> getIssues(
             @PathVariable String owner,
             @PathVariable String repo,
             @PathVariable int projectNumber,
             @RequestHeader("Authorization") String authHeader
     ) {
         return service.getIssues(owner, repo, projectNumber, authHeader.replace("Bearer ", ""))
-                .map(ResponseEntity::ok) // Успешный результат: HTTP 200 OK
+                .flatMap(response -> {
+                    try {
+                        // Предполагаем, что response.getBody() возвращает данные, которые нужно распарсить
+                        ObjectMapper objectMapper = new ObjectMapper();
+                        String json = objectMapper.writeValueAsString(response);
+                        List<TaskDTO> result = parser.parseIssues(json);
+                        return Mono.just(ResponseEntity.ok(result));
+                    } catch (JsonProcessingException e) {
+                        return Mono.error(e);
+                    }
+                })
                 .defaultIfEmpty(ResponseEntity.notFound().build()) // Если Mono пустой: HTTP 404 Not Found
                 .onErrorResume(e -> { // Обработка ошибок
                     System.err.println("Error fetching issues: " + e.getMessage());
@@ -61,10 +72,5 @@ public class GithubController {
                     System.out.println("Error fetching collaborators: "+e.getMessage());
                     return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
                 });
-    }
-
-    @PostMapping("/testsss")
-    public List<TaskDTO> parseJSON(@RequestBody String json) throws JsonProcessingException {
-        return parser.parseIssues(json);
     }
 }
