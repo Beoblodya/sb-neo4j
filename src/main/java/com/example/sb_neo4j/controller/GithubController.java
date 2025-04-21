c6dab3808b2e01a256810a52f956705da8219dbc
hprxkbrddd <egorushkinnikita65@gmail.com>
2025-04-21 08:21:50 +0300

    added request mapping

--- src/main/java/com/example/sb_neo4j/controller/GithubController.java
+++ src/main/java/com/example/sb_neo4j/controller/GithubController.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
+@RequestMapping("api/v1/GithubAPI")
 public class GithubController {
 
     private final GithubService service;
--- src/main/java/com/example/sb_neo4j/controller/PersonController.java
+++ src/main/java/com/example/sb_neo4j/controller/PersonController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("api/v1/Person")
 public class PersonController {
     @Autowired
     private final PersonService personService;
