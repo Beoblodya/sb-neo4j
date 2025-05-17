d50793b59ab5a01896582ee03e1f7c73f3355654
hprxkbrddd <egorushkinnikita65@gmail.com>
2025-05-17 22:22:37 +0300

    added skillSet field to Person class

--- src/main/java/com/example/sb_neo4j/model/Person.java
+++ src/main/java/com/example/sb_neo4j/model/Person.java
@@ -1,10 +1,11 @@
 package com.example.sb_neo4j.model;
 
-import org.springframework.aot.generate.GeneratedMethods;
-import org.springframework.aot.generate.GenerationContext;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Node
 public class Person {
@@ -12,6 +13,8 @@ public class Person {
     private Long id;
 
     private String name;
+
+    private List<String> skillSet = new ArrayList<>();
 
     public Person() {
     }
@@ -22,5 +25,13 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<String> getSkillSet() {
+        return skillSet;
+    }
+
+    public void setSkillSet(List<String> skillSet) {
+        this.skillSet = skillSet;
     }
 }
